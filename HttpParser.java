b0db8f90f2c730ab7f9b25c03340a7ef6f30a973
Andy J Rivera <rivera.andy40@gmail.com>
2020-09-28 18:23:48 -0400

    Update HttpParser.java

--- HttpParser.java
+++ HttpParser.java
@@ -13,44 +13,45 @@ public class HttpParser {
 	
 	//returns int to indicate status of request
 	public int parseRequest(String line) {
-		if(line == null || line.length() == 0) return -1; //considered malformed, but will probably never happen
-		if(Character.isWhitespace(line.charAt(0))) return -1; //considered malformed
-		
 		String[] parsedLine = line.split(" ");
-		if(parsedLine.length != 3) return -1;
+		if(parsedLine.length != 3) return -1; // 400 Bad Request
 		
-		//Command format checking MUST ACKNOWLEDGE OTHER EXISTING HTTP/1.0 COMMANDS 
-		if(parsedLine[0].equals("DELETE") || parsedLine[0].equals("PUT") || parsedLine[0].equals("LINK") || parsedLine[0].equals("UNLINK")) {	
-			return -2;	//command not supported
-		}else if(!parsedLine[0].equals("GET") || !parsedLine[0].equals("POST") || !parsedLine[0].equals("HEAD")) {
-			return -3; // bad request
+		//Command format checking
+		if(parsedLine[0].equals("get") || parsedLine[0].equals("post") || parsedLine[0].equals("head") ||
+				parsedLine[0].equals("delete") || parsedLine[0].equals("put") || parsedLine[0].equals("link") || parsedLine[0].equals("unlink")) {	
+			return -1;	// 400 Bad Request
+		}else if(parsedLine[0].equals("DELETE") || parsedLine[0].equals("PUT") || parsedLine[0].equals("LINK") || parsedLine[0].equals("UNLINK")) {
+			return -2; // 501 Not Implemented
+		}else if(!parsedLine[0].equals("GET") || !parsedLine[0].equals("POST") || !parsedLine[0].equals("HEAD")){
+			return -1; // 400 Bad Request
 		}else {
 			cmd = parsedLine[0];
 		}
 		
+		//Resource - Not sure if any format checking is necessary?
+		resource = parsedLine[1];
+		
 		//Version number format checking
-		if(parsedLine[2].indexOf("HTTP/") != 0 || parsedLine[2].indexOf(".") != 6 || !Character.isDigit(parsedLine[2].charAt(5)) ) {
-			return -1;	//malformed
+		if(parsedLine[2].indexOf("HTTP/") != 0 || !Character.isDigit(parsedLine[2].charAt(5)) ) {
+			return -1;	// 400 Bad Request
 		}else {
 			boolean decimal = false;
 			parsedLine[2] = parsedLine[2].substring(5);
 			for(int i=1; i < parsedLine[2].length(); i++) {	//we know first index is a digit
 				if(!Character.isDigit(parsedLine[2].charAt(i)) || parsedLine[2].charAt(i) != '.') {
-					return -1; //malformed
+					return -1; // 400 Bad Request - version number contains a alphabetic character instead of numeric
 				}else if(parsedLine[2].charAt(i) == '.' && decimal == true) {
-					return -1; //malformed
+					return -1; // 400 Bad Request - multiple decimals
 				}else if(parsedLine[2].charAt(i) == '.') {
 					decimal = true;
 				}
 			}
 			version = Double.parseDouble(parsedLine[2]);
 			if(version > 1.0) {
-				return -4; //version not supported
+				return -3; //505 version not supported
 			}
 		}
 		
-		//Resource format checking NOT YET IMPLEMENTED
-		resource = parsedLine[1];
 		return 0;
 		
 	}
