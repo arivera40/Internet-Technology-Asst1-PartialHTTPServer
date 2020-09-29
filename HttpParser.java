b85f41d02b57c71a88795f45d273beab89cf98f6
Andy J Rivera <rivera.andy40@gmail.com>
2020-09-28 23:57:48 -0400

    Update HttpParser.java

--- HttpParser.java
+++ HttpParser.java
@@ -1,14 +1,23 @@
 package test_HTTP;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class HttpParser {
 	private String cmd;
 	private String resource;
 	private double version;
 	
+	private File webroot;
+	
 	public HttpParser() {
 		resource = "";
 		cmd = "";
 		version = 0;
+		final File webroot = new File(".");
 	}
 	
 	//returns int to indicate status of request
@@ -48,12 +57,77 @@ public class HttpParser {
 			}
 			version = Double.parseDouble(parsedLine[2]);
 			if(version > 1.0) {
-				return -3; //505 version not supported
+				return -3; //version not supported
 			}
 		}
 		
 		return 0;
 		
+	}
+	
+	private String getMIME() {
+		if(resource.endsWith("html") || resource.endsWith("plain")) {
+			return "text/" + ((resource.endsWith("html")) ? "html" : "plain");
+		}else if(resource.endsWith("gif") || resource.endsWith("jpeg") || resource.endsWith("png")) {
+			return "image/" + ((resource.endsWith("gif")) ? "gif" : (resource.endsWith("jpeg")) ? "jpeg" : "png");
+		}else if(resource.endsWith("octet-stream") || resource.endsWith("pdf") || resource.endsWith("x-gzip") || resource.endsWith("zip")) {
+			return "application/" + ((resource.endsWith("octet-stream")) ? "octet-stream" : (resource.endsWith("pdf")) ? "pdf" :
+				(resource.endsWith("x-gzip")) ? "x-gzip" : "zip");
+		}else {
+			return "application/octet-stream";
+		}
+	}
+	
+	private byte[] getRequestedFile(File file, int length) {
+		byte[] data = new byte[length];
+		try {
+			FileInputStream fileInputStream = new FileInputStream(file);
+			fileInputStream.read(data);
+			fileInputStream.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return data;
+	}
+	
+	public void getHttpResponse(PrintWriter head, BufferedOutputStream body) {
+		File file = new File(webroot, resource);
+		if(file.exists() == false) {
+			head.println("HTTP/1.0 404 Not Found" + "\n\r");
+			head.flush();
+			return;
+		}
+		if(!file.canRead()) {
+			head.println("HTTP/1.0 403 Forbidden" + "\n\r");
+			head.flush();
+			return;
+		}
+		int contentLength = (int) file.length();
+		long lastModified = file.lastModified();
+		String type = getMIME();
+		
+		if(cmd.equals("GET") || cmd.equals("POST")) {
+			byte[] requestedFileData = getRequestedFile(file, contentLength);
+			head.println("HTTP/1.0 200 OK");
+			head.println("Content-Type: " + type);
+			head.println("Content-Length: " + contentLength);
+			head.println("Last-Modified: " + lastModified);	
+			head.println("Content-Encoding: identity" + "\n\r");
+			head.flush();
+			try {
+				body.write(requestedFileData, 0, contentLength);
+				body.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}			
+		}else {
+			head.println("HTTP/1.0 200 OK");
+			head.println("Content-Type: " + type);			
+			head.println("Content-Length: " + contentLength);
+			head.println("Last-Modified: " + lastModified);
+			head.println("Content-Encoding: identity" + "\n\r");
+			head.flush();
+		}
 	}
 	
 	public String getResource() {
