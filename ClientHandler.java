0cdcee0110a932cdfeaa18d5fecbebbb6f0a3d4c
Andy J Rivera <rivera.andy40@gmail.com>
2020-09-30 03:27:15 -0400

    Update ClientHandler.java

--- ClientHandler.java
+++ ClientHandler.java
@@ -27,48 +27,57 @@ public class ClientHandler extends Thread {
 	public void run() {
 		try {
 			// HTTP client requests are written to the InputStream
-			InputStreamReader inputStreamReader = new InputStreamReader(input_stream);
-			BufferedReader inStreamReader = new BufferedReader(inputStreamReader);
+			String request = getClientRequest();
 
 			// HTTP server responses are written into the OutputStream - we use PrintWriter to output response header
 			// and we use BufferedOutputStream to output response body/data when necessary
 			PrintWriter responseHead = new PrintWriter(output_stream);
 			BufferedOutputStream outStreamWriter = new BufferedOutputStream(output_stream);
 
-			String request;
-
 			// This will continue running until socket connection is closed
 			//Header may need to change to account for option If-Modified request (this would be in the following line)
-			while ((request = inStreamReader.readLine()) != null) {
-				HttpParser parser = new HttpParser();
-				int status = parser.parseRequest(request);
-				if (status == -1) {
-					// respond with 404 Bad Request
-					responseHead.println("HTTP/1.0 400 Bad Request" + "\n\r");
-					responseHead.flush();
 
-				} else if (status == -2) {
-					// respond with 501 Not Implemented
-					responseHead.println("HTTP/1.0 501 Not Implemented" + "\n\r");
-					responseHead.flush();
+			HttpParser parser = new HttpParser();
+			int status = parser.parseRequest(request);
+			if (status == -1) {
+				// respond with 404 Bad Request
+				responseHead.println("HTTP/1.0 400 Bad Request" + "\n\r");
+				responseHead.flush();
 
-				} else if (status == -3) {
-					// respond with HTTP Version Not Supported
-					responseHead.println("HTTP/1.0 505 HTTP Version Not Supported" + "\n\r");
-					responseHead.flush();
+			} else if (status == -2) {
+				// respond with 501 Not Implemented
+				responseHead.println("HTTP/1.0 501 Not Implemented" + "\n\r");
+				responseHead.flush();
 
-				} else {
-					parser.getHttpResponse(responseHead, outStreamWriter);
-					// Client request is valid and expects 200 OK, 403 Forbidden, 404 Not Found, or 304 Not Modified (If-Modified-Since)
-					// getHttpResponse(parser) parser.getCommand = command, parser.getResource =
-					// resource, parser.getVersion = version
-				}
+			} else if (status == -3) {
+				// respond with HTTP Version Not Supported
+				responseHead.println("HTTP/1.0 505 HTTP Version Not Supported" + "\n\r");
+				responseHead.flush();
 
+			} else {
+				parser.getHttpResponse(responseHead, outStreamWriter);
 			}
+			//wait quarter second
+			wait(250);
+			//PrintWriter and BufferedOutputStream already flush()'ed after use
+			//Close down communication object
+			responseHead.close();
+			outStreamWriter.close();
+			input_stream.close();
+			output_stream.close();
+			socket.close();
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+	}
+	
+	private String getClientRequest() throws IOException {
+		StringBuilder request = new StringBuilder();
+		do {
+			request.append((char) input_stream.read());
+		}while(input_stream.available() > 0);
+		return request.toString();
 	}
 }
