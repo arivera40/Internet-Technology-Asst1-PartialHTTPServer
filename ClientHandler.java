24e1f10a3a29e056bff52a74024951bd9464f523
Andy J Rivera <rivera.andy40@gmail.com>
2020-10-01 14:06:30 -0400

    Update ClientHandler.java

--- ClientHandler.java
+++ ClientHandler.java
@@ -1,12 +1,8 @@
 package test_HTTP;
 
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -26,12 +22,21 @@ public class ClientHandler extends Thread {
 	@Override
 	public void run() {
 		try {
-			// HTTP client requests are written to the InputStream
-			String request = getClientRequest();
-
 			// HTTP server responses are written into the OutputStream - we use PrintWriter to output response header
 			// and we use BufferedOutputStream to output response body/data when necessary
 			PrintWriter responseHead = new PrintWriter(output_stream);
+		
+			String request = getClientRequest(responseHead);
+			//if no incoming requests within 5 seconds, request will equal the empty string
+			if(request.equals("")) {
+				wait(250);
+				responseHead.close();
+				input_stream.close();
+				output_stream.close();
+				socket.close();
+				return;
+			}
+			
 			BufferedOutputStream outStreamWriter = new BufferedOutputStream(output_stream);
 
 			// This will continue running until socket connection is closed
@@ -73,10 +78,25 @@ public class ClientHandler extends Thread {
 
 	}
 	
-	private String getClientRequest() throws IOException {
+	private String getClientRequest(PrintWriter response) throws IOException {
 		StringBuilder request = new StringBuilder();
+		boolean first = true;
 		do {
+			if(first) {
+				try {
+					wait(5000);
+					if(input_stream.available() <= 0) {
+						response.println("HTTP/1.0 505 HTTP Version Not Supported" + "\n\r");
+						response.flush();
+						return "";
+						
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 			request.append((char) input_stream.read());
+			first = false;
 		}while(input_stream.available() > 0);
 		return request.toString();
 	}
